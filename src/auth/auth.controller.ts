bd2ec289694374f5a643d702a9137563ea44f281
Sebastian Velasco <sesbas778@gmail.com>
2025-08-12 09:12:48 -0500

    feat(auth): add Transformer and Validator classes with register/login DTOs
    - Implemented Transformer class for data mapping
    - Implemented Validator class for input validation
    - Added configuration for Transformer and Validator
    - Created RegisterDto and LoginDto for authentication requests

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,20 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @MessagePattern('auth.register.user')
-  registerUser() {
-    return 'auth.register.user';
+  registerUser(@Payload() registerUserDto: RegisterUserDto) {
+    return registerUserDto;
   }
 
   @MessagePattern('auth.login.user')
-  loginUser() {
-    return 'auth.login.user';
+  loginUser(@Payload() loginUserDto: LoginUserDto) {
+    return loginUserDto;
   }
 
   @MessagePattern('auth.verify.user')
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config';
 import { AppModule } from './app.module';
@@ -12,6 +12,12 @@ async function bootstrap() {
       transport: Transport.NATS,
       options: { servers: envs.natsServers },
     },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
   await app.listen();
   logger.log(`Auth Microservice running or port ${envs.port}`);
